9733f45f204f7f1042f2fad3d0444c9f3dbb7b11
Connor Zanin <cnnrznn@gmail.com>
2023-10-03 01:42:51 -0600

    DB Connection logic and service skeletons

--- play/play.go
+++ play/play.go
@@ -1,6 +1,23 @@
 package play
 
-import "github.com/cnnrznn/playtogether/model"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+
+	"github.com/cnnrznn/playtogether/model"
+)
+
+const (
+	DB_CONN = "hostname=localhost user=pt database=playtogether sslmode=disable"
+)
+
+type PlayService struct {
+	db       *sql.DB
+	initDone bool
+}
 
 func Update(ping model.Ping) error {
 	// First, check for games already going on in the area
@@ -12,8 +29,35 @@ func Update(ping model.Ping) error {
 	return nil
 }
 
-type PlayService struct{}
+func (s *PlayService) Init() error {
+	if !s.initDone {
+		conn, err := sql.Open(
+			"postgres",
+			fmt.Sprintf(
+				"%v password='%v'", DB_CONN, os.Getenv("DB_PASSWD"),
+			),
+		)
+		if err != nil {
+			return err
+		}
 
-func (s *PlayService) Run() {
+		s.db = conn
+	}
+
+	return nil
+}
+
+func (s *PlayService) Run() error {
 	// every 5m, scan and delete rows in db past expiration
+	if err := s.Init(); err != nil {
+		return err
+	}
+
+	go s.runExpire()
+
+	return nil
+}
+
+func (s *PlayService) runExpire() error {
+	return nil
 }
