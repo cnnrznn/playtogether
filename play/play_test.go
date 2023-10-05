42e50f1ad0baee45d5e561146f922525caf9f51b
Connor Zanin <cnnrznn@gmail.com>
2023-10-05 16:11:18 -0600

    Test ping

--- play/play_test.go
+++ play/play_test.go
@@ -3,8 +3,11 @@ package play
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/cnnrznn/playtogether/model"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAreaCalculation(t *testing.T) {
@@ -25,4 +28,22 @@ func TestAreaCalculation(t *testing.T) {
 
 	areaCO := calculateArea(pingCO)
 	fmt.Println(areaCO)
+}
+
+func TestPing(t *testing.T) {
+	ping := model.Ping{
+		Player:   uuid.New(),
+		Activity: "volleyball",
+		Lat:      40.0150,
+		Lon:      105.2705,
+		Expire:   int(time.Now().Unix()),
+		RangeKM:  10,
+	}
+
+	res, err := Update(ping)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, res.Found, false)
 }
